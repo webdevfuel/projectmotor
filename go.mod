cf6b3197e144e28847a0c1f39f13dba5ed37fc7e
Emanuel Turis <hello@emanuelturis.com>
2023-12-28 16:26:33 +0000

    routing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/webdevfuel/projectmotor
 
 go 1.21.5
+
+require github.com/go-chi/chi/v5 v5.0.11 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "log"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	log.Println("Hello, world!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+	http.ListenAndServe("localhost:3000", r)
 }
+
