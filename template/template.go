34fd92bfcb16f7b1dc7a7ce06b3f5476b57552c4
Emanuel Turis <hello@emanuelturis.com>
2023-12-30 21:54:32 +0000

    add login page to template vars

--- template/template.go
+++ template/template.go
@@ -7,4 +7,6 @@ var set = pongo2.NewSet("default", pongo2.MustNewLocalFileSystemLoader("./views"
 var (
 	// Dashboard
 	Dashboard *pongo2.Template = pongo2.Must(set.FromFile("dashboard/index.html"))
+	// Login
+	Login *pongo2.Template = pongo2.Must(set.FromFile("login/index.html"))
 )
