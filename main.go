a0d21d8790d91f84cc6e1d74969e4ac6a354569a
Emanuel Turis <hello@emanuelturis.com>
2023-12-30 17:30:33 +0000

    server static files

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	fs := http.FileServer(http.Dir("./static"))
+	r.Handle("/static/*", http.StripPrefix("/static/", fs))
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		template.Dashboard.ExecuteWriter(pongo2.Context{}, w)
 	})
