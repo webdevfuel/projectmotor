984aa02c19e862682a6ad423b266e83459b9def5
Emanuel Turis <hello@emanuelturis.com>
2023-12-31 21:53:28 +0000

    handle errors with fail fn

--- handler/handler.go
+++ handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/sessions"
@@ -28,4 +29,9 @@ func NewHandler(options HandlerOptions) *Handler {
 
 func (h Handler) GetSessionStore(r *http.Request) (*sessions.Session, error) {
 	return h.store.Get(r, "_projectmotor_session")
+}
+
+func fail(w http.ResponseWriter, err error, code int) {
+	http.Error(w, http.StatusText(code), code)
+	log.Println("error:", err)
 }
