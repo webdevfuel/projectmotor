35952e6ae31b8e617400f6bb5e2f467c5340a010
Emanuel Turis <hello@emanuelturis.com>
2023-12-31 16:17:30 +0000

    pass db to handler options

--- main.go
+++ main.go
@@ -7,28 +7,30 @@ import (
 	"github.com/flosch/pongo2/v6"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/webdevfuel/projectmotor/db"
+	"github.com/webdevfuel/projectmotor/database"
 	"github.com/webdevfuel/projectmotor/handler"
 	"github.com/webdevfuel/projectmotor/template"
 )
 
 func main() {
-	err := db.OpenDB()
+	db, err := database.OpenDB()
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = db.SetupDB()
+	err = database.SetupDB()
 	if err != nil {
 		log.Fatal(err)
 	}
-	h := handler.NewHandler(handler.HandlerOptions{})
+	h := handler.NewHandler(handler.HandlerOptions{
+		DB: db,
+	})
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	fs := http.FileServer(http.Dir("./static"))
 	r.Handle("/static/*", http.StripPrefix("/static/", fs))
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		// NOTE ->> only for testing, remove after actual interactions with database
-		message, err := db.GetMessage()
+		message, err := database.GetMessage()
 		if err != nil {
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			return
