a19ac184c6e699059bed624aba043ad577dfa75b
Emanuel Turis <hello@emanuelturis.com>
2023-12-31 16:01:16 +0000

    set up user model and service

---
+++ db/user.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/jmoiron/sqlx"
+)
+
+type User struct {
+	ID     int32       `json:"id"`
+	Name   pgtype.Text `json:"name"`
+	Email  string      `json:"email"`
+	Avatar pgtype.Text `json:"avatar"`
+}
+
+type UserService struct {
+	db *sqlx.DB
+}
+
+func NewUserService(db *sqlx.DB) *UserService {
+	return &UserService{
+		db: db,
+	}
+}
