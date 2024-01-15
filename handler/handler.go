e828074969d81a06f222dd3b7a4da6c680b3a5cc
Emanuel Turis <hello@emanuelturis.com>
2024-01-15 19:22:56 +0000

    add handler method to begin db transaction

--- handler/handler.go
+++ handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"context"
 	"log"
 	"net/http"
 
@@ -13,6 +14,7 @@ type Handler struct {
 	userService    *database.UserService
 	accountService *database.AccountService
 	store          *sessions.CookieStore
+	db             *sqlx.DB
 }
 
 type HandlerOptions struct {
@@ -25,6 +27,7 @@ func NewHandler(options HandlerOptions) *Handler {
 	accountService := database.NewAccountService(options.DB)
 	return &Handler{
 		store:          options.Store,
+		db:             options.DB,
 		userService:    userService,
 		accountService: accountService,
 	}
@@ -32,6 +35,14 @@ func NewHandler(options HandlerOptions) *Handler {
 
 func (h Handler) GetSessionStore(r *http.Request) (*sessions.Session, error) {
 	return h.store.Get(r, "_projectmotor_session")
+}
+
+func (h Handler) BeginTx(ctx context.Context) (*sqlx.Tx, error) {
+	tx, err := h.db.BeginTxx(ctx, nil)
+	if err != nil {
+		return &sqlx.Tx{}, err
+	}
+	return tx, nil
 }
 
 func fail(w http.ResponseWriter, err error, code int) {
