dbf954f373af608f663eeec52573aea51578354e
Emanuel Turis <hello@emanuelturis.com>
2024-01-15 22:17:59 +0000

    user context key and set user session

--- auth/auth.go
+++ auth/auth.go
@@ -1,3 +1,18 @@
 package auth
 
-type UserIDKey struct{}
+import (
+	"net/http"
+
+	"github.com/gorilla/sessions"
+)
+
+type UserKey struct{}
+
+func SetUserSession(w http.ResponseWriter, r *http.Request, userID int32, session *sessions.Session) error {
+	// Clear previous state and code from session values
+	delete(session.Values, "state")
+	delete(session.Values, "code")
+	// Store new user on session values
+	session.Values["userID"] = userID
+	return session.Save(r, w)
+}
