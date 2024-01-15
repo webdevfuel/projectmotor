d07c80be0802708521266b052ffb16c3dcb2aab6
Emanuel Turis <hello@emanuelturis.com>
2024-01-15 19:22:46 +0000

    add user methods

--- database/user.go
+++ database/user.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"database/sql"
+
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jmoiron/sqlx"
 )
@@ -20,4 +22,27 @@ func NewUserService(db *sqlx.DB) *UserService {
 	return &UserService{
 		db: db,
 	}
+}
+
+func (us UserService) GetUserByID(ID int32) (User, bool, error) {
+	var user User
+	query := "select * from users where id = $1"
+	err := us.db.Get(&user, query, ID)
+	if err != sql.ErrNoRows {
+		if err != nil {
+			return User{}, false, err
+		}
+		return user, true, nil
+	}
+	return User{}, false, nil
+}
+
+func (us UserService) CreateUser(tx *sqlx.Tx, email string) (User, error) {
+	var user User
+	query := "insert into users (email) values ($1) returning *"
+	err := tx.Get(&user, query, email)
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
 }
