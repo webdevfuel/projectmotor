3e4b7e49aba57e0f509ad6291aadc49fd1775739
Emanuel Turis <hello@emanuelturis.com>
2024-01-15 19:22:35 +0000

    add account methods and update struct keys

--- database/account.go
+++ database/account.go
@@ -1,11 +1,15 @@
 package database
 
-import "github.com/jmoiron/sqlx"
+import (
+	"database/sql"
+
+	"github.com/jmoiron/sqlx"
+)
 
 type Account struct {
-	UserID      int32  `json:"userId"`
-	AccountID   string `json:"accountId"`
-	AccessToken string `json:"accessToken"`
+	ID          string `json:"id" db:"id"`
+	UserID      int32  `json:"userId" db:"user_id"`
+	AccessToken string `json:"accessToken" db:"access_token"`
 }
 
 type AccountService struct {
@@ -16,4 +20,27 @@ func NewAccountService(db *sqlx.DB) *AccountService {
 	return &AccountService{
 		db: db,
 	}
+}
+
+func (as AccountService) GetAccountByID(ID int32) (Account, bool, error) {
+	var account Account
+	query := "select * from accounts where id = $1"
+	err := as.db.Get(&account, query, ID)
+	if err != sql.ErrNoRows {
+		if err != nil {
+			return Account{}, false, err
+		}
+		return account, true, nil
+	}
+	return Account{}, false, nil
+}
+
+func (as AccountService) CreateAccount(tx *sqlx.Tx, ID int32, userID int32, accessToken string) (Account, error) {
+	var account Account
+	query := "insert into accounts (id, user_id, access_token) values ($1, $2, $3) returning *"
+	err := tx.Get(&account, query, ID, userID, accessToken)
+	if err != nil {
+		return Account{}, err
+	}
+	return account, nil
 }
