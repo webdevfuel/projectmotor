24a2b31466f71d377e0327f8bc5f3819354980b0
Emanuel Turis <hello@emanuelturis.com>
2024-01-28 06:01:34 +0000

    list projects with get request to /projects

--- main.go
+++ main.go
@@ -45,6 +45,7 @@ func main() {
 func protectedRouter(h *handler.Handler) func(chi.Router) {
 	return func(r chi.Router) {
 		r.Use(ProtectedCtx(h))
+		r.Get("/projects", h.GetProjects)
 		r.Get("/", h.Dashboard)
 	}
 }
