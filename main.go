3d0b7a8ff7303d6cdad4da834be4b4bd40ae1e6b
Emanuel Turis <hello@emanuelturis.com>
2024-01-30 04:07:01 +0000

    add new project route to router

--- main.go
+++ main.go
@@ -46,6 +46,7 @@ func protectedRouter(h *handler.Handler) func(chi.Router) {
 	return func(r chi.Router) {
 		r.Use(ProtectedCtx(h))
 		r.Get("/projects", h.GetProjects)
+		r.Get("/projects/new", h.NewProject)
 		r.Get("/", h.Dashboard)
 	}
 }
