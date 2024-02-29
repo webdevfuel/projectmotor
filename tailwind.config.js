4e13aa020532d71d11a23bb7dcb814d7e1fca0ce
Emanuel Turis <hello@emanuelturis.com>
2024-02-29 00:46:33 +0000

    make changes tailwind for templ migration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./views/**/*.html"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
+    content: [
+        "./template/**/*.templ"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/forms')
+    ],
 }
 
