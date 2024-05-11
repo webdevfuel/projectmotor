6d2b99b1c465137062ddb007a628dc9ad2939ee8
Emanuel Turis <hello@emanuelturis.com>
2024-05-11 17:27:49 +0300

    add setup script to download js files

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir ./static
+
+# HTMX
+wget https://unpkg.com/htmx.org/dist/htmx.min.js -O ./static/htmx.min.js
+
+# Alpine.js
+wget https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js -O ./static/alpine.min.js
+
+# Preline UI
+wget https://raw.githubusercontent.com/htmlstreamofficial/preline/main/dist/dropdown.js -O ./static/dropdown.js
