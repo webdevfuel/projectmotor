551801add49610ba99cdc99e757def2c9db1d7c5
Emanuel Turis <hello@emanuelturis.com>
2024-05-14 18:53:57 +0300

    comment account package

--- database/account.go
+++ database/account.go
@@ -6,22 +6,35 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+// An Account relates OAuth providers to users, to enable one user associating
+// with multiple OAuth providers and accounts in the future.
 type Account struct {
-	ID          string `json:"id" db:"id"`
-	UserID      int32  `json:"userId" db:"user_id"`
+	// ID is the primary key of the "accounts" table.
+	ID string `json:"id" db:"id"`
+	// UserID is a foreign key to the "users" table.
+	UserID int32 `json:"userId" db:"user_id"`
+	// AccessToken is the access token provided by the OAuth provider.
 	AccessToken string `json:"accessToken" db:"access_token"`
 }
 
+// An AccountService is a connection to the database with methods
+// for interacting with the "accounts" table.
 type AccountService struct {
 	db *sqlx.DB
 }
 
+// NewAccountService returns a pointer to AccountService.
 func NewAccountService(db *sqlx.DB) *AccountService {
 	return &AccountService{
 		db: db,
 	}
 }
 
+// GetAccountByID returns an Account, reports whether the account exists inside
+// the database with the given id, and returns an error from the Get method.
+//
+// sql.ErrNoRows isn't treated as an error, but rather makes the function
+// report the account doesn't exist.
 func (as AccountService) GetAccountByID(ID int32) (Account, bool, error) {
 	var account Account
 	query := "select * from accounts where id = $1"
@@ -35,6 +48,9 @@ func (as AccountService) GetAccountByID(ID int32) (Account, bool, error) {
 	return Account{}, false, nil
 }
 
+// CreateAccount returns an Account and returns an error from the Get method.
+//
+// If successful, it inserts a new row into the "accounts" table with the given data.
 func (as AccountService) CreateAccount(tx *sqlx.Tx, ID int32, userID int32, accessToken string) (Account, error) {
 	var account Account
 	query := "insert into accounts (id, user_id, access_token) values ($1, $2, $3) returning *"
