b43bd92958d9444a6f85512dbffd83868af28c09
Emanuel Turis <hello@emanuelturis.com>
2024-05-14 19:44:51 +0300

    comment project package

--- database/project.go
+++ database/project.go
@@ -5,27 +5,46 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+// A Project enables splitting tasks into different categories and
+// sharing them all at once with other users.
 type Project struct {
-	ID          int32            `db:"id"`
-	Title       string           `db:"title"`
-	Description pgtype.Text      `db:"description"`
-	Published   bool             `db:"published"`
-	OwnerID     int32            `db:"owner_id"`
-	CreatedAt   pgtype.Timestamp `db:"created_at"`
-	UpdatedAt   pgtype.Timestamp `db:"updated_at"`
-	Shared      bool             `db:"-"`
+	// ID is the primary key of the "projects" table.
+	ID int32 `db:"id"`
+	// Title is the main identifier of a project.
+	Title string `db:"title"`
+	// Description is an optional text to describe the project.
+	Description pgtype.Text `db:"description"`
+	// Published reports whether the project is available with shared users.
+	Published bool `db:"published"`
+	// OwnerID is a foreign key to the "users" table.
+	OwnerID int32 `db:"owner_id"`
+	// CreatedAt tracks when the project was created by the owner.
+	CreatedAt pgtype.Timestamp `db:"created_at"`
+	// UpdatedAt tracks the last time the project was updated by the owner.
+	UpdatedAt pgtype.Timestamp `db:"updated_at"`
+	// Shared reports whether the project is shared or owned.
+	// It should always be set by Go code, and doesn't map to
+	// any column inside the "projects" table.
+	Shared bool `db:"-"`
 }
 
+// An ProjectService is a connection to the database with methods
+// for interacting with the "projects" table.
 type ProjectService struct {
 	db *sqlx.DB
 }
 
+// NewProjectService returns a pointer to ProjectService.
 func NewProjectService(db *sqlx.DB) *ProjectService {
 	return &ProjectService{
 		db: db,
 	}
 }
 
+// Create returns a Project and returns an error from the Get method.
+//
+// If successful, it inserts a new row into the "projects" table with
+// the given title and description.
 func (s ProjectService) Create(title string, description string, ownerID int32) (Project, error) {
 	var project Project
 	err := s.db.Get(&project, "insert into projects (title, description, owner_id) values ($1, $2, $3) returning *", title, description, ownerID)
@@ -35,6 +54,10 @@ func (s ProjectService) Create(title string, description string, ownerID int32) 
 	return project, nil
 }
 
+// TogglePublished returns a Project and returns an error from the Get method.
+//
+// If successful, it updates the "published" column inside the "projects" table
+// by the given id, to the opposite of the previous value.
 func (s ProjectService) TogglePublished(projectID int32, ownerID int32) (Project, error) {
 	var project Project
 	err := s.db.Get(&project, "update projects set published = not published where id = $1 and owner_id = $2 returning *", projectID, ownerID)
@@ -44,6 +67,7 @@ func (s ProjectService) TogglePublished(projectID int32, ownerID int32) (Project
 	return project, nil
 }
 
+// Get returns a Project and returns an error from the Get method.
 func (s ProjectService) Get(projectID int32, ownerID int32) (Project, error) {
 	var project Project
 	err := s.db.Get(&project, "select * from projects where id = $1 and owner_id = $2", projectID, ownerID)
@@ -53,6 +77,7 @@ func (s ProjectService) Get(projectID int32, ownerID int32) (Project, error) {
 	return project, nil
 }
 
+// GetAll returns a slice of Project and returns an error from the Select method.
 func (s ProjectService) GetAll(ownerID int32) ([]Project, error) {
 	var projects []Project
 	query := "select * from projects where owner_id = $1 order by created_at desc"
@@ -63,6 +88,10 @@ func (s ProjectService) GetAll(ownerID int32) ([]Project, error) {
 	return projects, nil
 }
 
+// Update returns a Project and returns an error from the Get method.
+//
+// If successful, it updates the "projects" table row that matches the
+// given project id and owner id, with the given title and description.
 func (s ProjectService) Update(projectID int32, title string, description string, ownerID int32) (Project, error) {
 	var project Project
 	err := s.db.Get(&project, "update projects set title = $1, description = $2 where id = $3 and owner_id = $4 returning *", title, description, projectID, ownerID)
@@ -72,6 +101,10 @@ func (s ProjectService) Update(projectID int32, title string, description string
 	return project, nil
 }
 
+// Delete returns an error from the Exec method.
+//
+// If successful, it delete the "projects" table row that matches the
+// given project id and owner id.
 func (s ProjectService) Delete(projectID int32, ownerID int32) error {
 	_, err := s.db.Exec("delete from projects where id = $1 and owner_id = $2", projectID, ownerID)
 	if err != nil {
