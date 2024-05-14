20493c752b1e5cc34328f5c6cde3732935f27f1d
Emanuel Turis <hello@emanuelturis.com>
2024-05-14 18:39:29 +0300

    comment auth and validator packages

--- auth/auth.go
+++ auth/auth.go
@@ -6,13 +6,23 @@ import (
 	"github.com/gorilla/sessions"
 )
 
+// A UserKey is the representation of a key, for usage with context.
+//
+// An example of usage to set an user inside a context:
+//
+//	user := getUserById(0)
+//	ctx := context.Background()
+//	ctx = context.WithValue(ctx, auth.UserKey{}, user)
 type UserKey struct{}
 
+// SetUserSession returns an error when calling session.Save(r, w).
+//
+// It clears the previous values from the given session with keys "state" and "code".
+//
+// It sets the given userID on values with key "userID".
 func SetUserSession(w http.ResponseWriter, r *http.Request, userID int32, session *sessions.Session) error {
-	// Clear previous state and code from session values
 	delete(session.Values, "state")
 	delete(session.Values, "code")
-	// Store new user on session values
 	session.Values["userID"] = userID
 	return session.Save(r, w)
 }
--- validator/validator.go
+++ validator/validator.go
@@ -11,18 +11,26 @@ import (
 
 var decoder *form.Decoder
 
+// A Validator is a struct of any type with a Validate method.
 type Validator interface {
 	Validate() error
 }
 
+// A Validated is a struct that holds the key, value and error strings.
+// Key is used to grab a Validated instance inside templates, and should always be PascalCase.
+// Value is used to pass to the client the previously available input value.
+// Error is used to pass to the client a human-readable message.
 type Validated struct {
 	Key   string
 	Value string
 	Error string
 }
 
+// A ValidatedSlice is an alias to []Validated.
 type ValidatedSlice []Validated
 
+// GetByKey returns a Validated struct from a slice,
+// and should always be PascalCase.
 func (vs ValidatedSlice) GetByKey(key string) Validated {
 	for _, v := range vs {
 		if v.Key == key {
@@ -32,10 +40,20 @@ func (vs ValidatedSlice) GetByKey(key string) Validated {
 	return Validated{}
 }
 
+// NewValidatedSlice returns a ValidatedSlice.
 func NewValidatedSlice() ValidatedSlice {
 	return []Validated{}
 }
 
+// Validate reports whether the type implementing the Validator interface is
+// valid, according to validation rules specified within Validate method.
+//
+// It also returns a ValidatedSlice with all information about the invalid
+// type and the first error encountered while trying to validating.
+//
+// The error only occurs if there was an internal problem with the function,
+// and bool should be used to track whether the validation was successful
+// according to the rules.
 func Validate(v Validator, r *http.Request) (bool, ValidatedSlice, error) {
 	decoder = form.NewDecoder()
 	err := r.ParseForm()
@@ -78,28 +96,28 @@ func parseErrors(errors validation.Errors, data any) []Validated {
 	return vmap
 }
 
-type KeyValue struct {
+type keyValue struct {
 	Key   string
 	Value string
 }
 
-func getKeyValue(val reflect.Value, fieldName string, i int) []KeyValue {
+func getKeyValue(val reflect.Value, fieldName string, i int) []keyValue {
 	switch val.Field(i).Kind() {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return []KeyValue{{
+		return []keyValue{{
 			Key:   fieldName,
 			Value: fmt.Sprintf("%d", val.Field(i).Int()),
 		}}
 	case reflect.String:
-		return []KeyValue{{
+		return []keyValue{{
 			Key:   fieldName,
 			Value: val.Field(i).String(),
 		}}
 	case reflect.Bool:
-		return []KeyValue{{
+		return []keyValue{{
 			Key:   fieldName,
 			Value: fmt.Sprintf("%t", val.Field(i).Bool()),
 		}}
 	}
-	return []KeyValue{}
+	return []keyValue{}
 }
