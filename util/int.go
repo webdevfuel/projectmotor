4c2b195905238871e3283d84ed92009da1d59c90
Emanuel Turis <hello@emanuelturis.com>
2024-05-16 17:40:39 +0300

    comment util package

--- util/int.go
+++ util/int.go
@@ -2,6 +2,10 @@ package util
 
 import "strconv"
 
+// Atoi32 parses the given string and returns an int32 and an error
+// from the strconv.Atoi method.
+//
+// It wraps the returned int from the strconv.Atoi method in the int32() function.
 func Atoi32(s string) (int32, error) {
 	id, err := strconv.Atoi(s)
 	if err != nil {
