2e93a985492e3d88b6d5b621e49304d1189c05be
Emanuel Turis <hello@emanuelturis.com>
2024-05-16 15:08:37 +0300

    comment user package

--- database/user.go
+++ database/user.go
@@ -7,6 +7,14 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+// A User is where we keep the information about a person, with the unique
+// point of data being the email address.
+//
+// The email address is not set by the user when signing up, but rather
+// extracted from the OAuth provider if a user doesn't yet exist with
+// the email address sent by the OAuth provider.
+//
+// table: "users"
 type User struct {
 	ID     int32       `json:"id"`
 	Name   pgtype.Text `json:"name"`
@@ -14,16 +22,24 @@ type User struct {
 	Avatar pgtype.Text `json:"avatar"`
 }
 
+// A UserService is a connection to the database with methods
+// for interacting with the "users" table.
 type UserService struct {
 	db *sqlx.DB
 }
 
+// NewUserService returns a pointer to UserService.
 func NewUserService(db *sqlx.DB) *UserService {
 	return &UserService{
 		db: db,
 	}
 }
 
+// GetAccountByID returns a User, reports whether the user exists inside
+// the database with the given id, and returns an error from the Get method.
+//
+// sql.ErrNoRows isn't treated as an error, but rather makes the function
+// report the user doesn't exist.
 func (us UserService) GetUserByID(ID int32) (User, bool, error) {
 	var user User
 	query := "select * from users where id = $1"
@@ -37,6 +53,9 @@ func (us UserService) GetUserByID(ID int32) (User, bool, error) {
 	return User{}, false, nil
 }
 
+// CreateUser returns a User and returns an error from the Get method.
+//
+// If successful, it inserts a new row into the "users" table with the given data.
 func (us UserService) CreateUser(tx *sqlx.Tx, email string) (User, error) {
 	var user User
 	query := "insert into users (email) values ($1) returning *"
