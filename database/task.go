1c2be5cdba49b57702457b6a2587e304067a812f
Emanuel Turis <hello@emanuelturis.com>
2024-05-16 14:59:45 +0300

    comment task package

--- database/task.go
+++ database/task.go
@@ -5,6 +5,10 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+// A Task is a way for users to keep a title and helpful description of a
+// thing they want to do, along with a completed state.
+//
+// table: "tasks"
 type Task struct {
 	ID          int32            `db:"id"`
 	Title       string           `db:"title"`
@@ -15,16 +19,22 @@ type Task struct {
 	UpdatedAt   pgtype.Timestamp `db:"updated_at"`
 }
 
+// A TaskService is a connection to the database with methods
+// for interacting with the "tasks" table.
 type TaskService struct {
 	db *sqlx.DB
 }
 
+// NewTaskService returns a pointer to TaskService.
 func NewTaskService(db *sqlx.DB) *TaskService {
 	return &TaskService{
 		db: db,
 	}
 }
 
+// Create returns a Task and returns an error from the Get method.
+//
+// If successful, it inserts a new row into the "tasks" table with the given data.
 func (s *TaskService) Create(title string, description string, projectID int32, ownerID int32) error {
 	var task Task
 	return s.db.Get(&task, `
@@ -35,6 +45,7 @@ func (s *TaskService) Create(title string, description string, projectID int32, 
 	`, title, description, ownerID, projectID)
 }
 
+// GetAll returns a slice of Task and returns an error from the Select method.
 func (s *TaskService) GetAll(ownerID int32) ([]Task, error) {
 	var tasks []Task
 	err := s.db.Select(&tasks, `
@@ -48,6 +59,9 @@ func (s *TaskService) GetAll(ownerID int32) ([]Task, error) {
 	return tasks, err
 }
 
+// GetAll returns a slice of Task and returns an error from the Select method.
+//
+// It filters the query by the given project id.
 func (s *TaskService) GetAllByProjectID(ownerID int32, projectID int32) ([]Task, error) {
 	var tasks []Task
 	err := s.db.Select(&tasks, `
@@ -62,6 +76,7 @@ func (s *TaskService) GetAllByProjectID(ownerID int32, projectID int32) ([]Task,
 	return tasks, err
 }
 
+// Get returns a Task and returns an error from the Get method.
 func (s *TaskService) Get(taskID int32, ownerID int32) (Task, error) {
 	var task Task
 	err := s.db.Get(&task, `
@@ -76,6 +91,10 @@ func (s *TaskService) Get(taskID int32, ownerID int32) (Task, error) {
 	return task, err
 }
 
+// Update returns a Task and returns an error from the Get method.
+//
+// If successful, it updates the "tasks" table row that matches the
+// given task id and owner id, with the given title and description.
 func (s *TaskService) Update(taskID int32, ownerID int32, title string, description string) error {
 	_, err := s.db.Exec(`
 		UPDATE
