e0e13896f9b1ba9888f9a9f4c992068905583e71
Emanuel Turis <hello@emanuelturis.com>
2024-05-17 20:05:19 +0300

    add breakpoints to seeds

--- database/seeds/001_user.sql
+++ database/seeds/001_user.sql
@@ -1,12 +1,15 @@
 INSERT INTO users ("id", "name", "email", "avatar")
     VALUES (1, 'Web Dev Fuel', 'hello@webdevfuel.com', '');
 
+--> statement-breakpoint
 INSERT INTO accounts ("id", "user_id", "access_token")
     VALUES (1, 1, 'REDACTED');
 
+--> statement-breakpoint
 INSERT INTO users ("id", "name", "email", "avatar")
     VALUES (2, 'John Doe', 'johndoe@gmail.com', '');
 
+--> statement-breakpoint
 INSERT INTO accounts ("id", "user_id", "access_token")
     VALUES (2, 2, 'REDACTED');
 
