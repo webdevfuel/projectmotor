f0969b6caed08d540ae424f8f6499a633368bdc1
Emanuel Turis <hello@emanuelturis.com>
2024-05-17 16:31:22 +0300

    improve comment

--- test/session.go
+++ test/session.go
@@ -8,7 +8,7 @@ import (
 )
 
 // SetTestUserSession returns a cookie with the format `_projectmotor_session=%s;`
-// where %s is the session string with a user.
+// where %s is the session string with data about a user.
 //
 // It uses the test session.CookieStore and stores the given userId with key "userID".
 func SetTestUserSession(userId int) (string, error) {
