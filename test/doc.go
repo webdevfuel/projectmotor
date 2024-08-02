4b7fa681ca1bd27f88a955f4426d302aed4c6b52
Emanuel Turis <hello@emanuelturis.com>
2024-08-02 20:37:07 +0300

    find inside document by selector and text

--- test/doc.go
+++ test/doc.go
@@ -12,3 +12,18 @@ func Doc(res *http.Response) *goquery.Document {
 	doc, _ := goquery.NewDocumentFromReader(res.Body)
 	return doc
 }
+
+// FindByText returns a goquery.Selection with the provided selector,
+// filtered by the provided text.
+//
+// It first finds all elements that match the selector, and then inside
+// the Each function uses the Text method to compare against provided text.
+func FindByText(doc *goquery.Document, selector string, text string) *goquery.Selection {
+	var ts *goquery.Selection
+	doc.Find(selector).Each(func(_ int, cs *goquery.Selection) {
+		if cs.Text() == text {
+			ts = cs
+		}
+	})
+	return ts
+}
