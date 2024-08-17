4d9e4e93f513a430efe7a97ccf4c3aa06717aedb
Emanuel Turis <hello@emanuelturis.com>
2024-08-17 17:33:45 +0300

    allow nullable project id

--- database/migrations/000003_task.up.sql
+++ database/migrations/000003_task.up.sql
@@ -4,9 +4,9 @@ CREATE TABLE tasks (
     "description" text,
     "created_at" timestamp NOT NULL DEFAULT now(),
     "updated_at" timestamp NOT NULL DEFAULT now(),
-    "project_id" integer NOT NULL,
+    "project_id" integer,
     "owner_id" integer NOT NULL,
-    CONSTRAINT fk_project FOREIGN KEY (project_id) REFERENCES projects (id),
+    CONSTRAINT fk_project FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE SET NULL,
     CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES users (id)
 );
 
