b988fcfe8fc4dfe37521b7cabd75241d64223d33
Emanuel Turis <hello@emanuelturis.com>
2024-08-17 15:09:42 +0300

    add task seeds

---
+++ database/seeds/003_task.sql
@@ -0,0 +1,31 @@
+INSERT INTO tasks (title, description, project_id, owner_id)
+    VALUES ('Task 1', '', 1, 1);
+
+--> statement-breakpoint
+INSERT INTO tasks (title, description, project_id, owner_id)
+    VALUES ('Task 2', '', 1, 1);
+
+--> statement-breakpoint
+INSERT INTO tasks (title, description, project_id, owner_id)
+    VALUES ('Task 3', '', 2, 1);
+
+--> statement-breakpoint
+INSERT INTO tasks (title, description, project_id, owner_id)
+    VALUES ('Task 4', '', 2, 1);
+
+--> statement-breakpoint
+INSERT INTO tasks (title, description, project_id, owner_id)
+    VALUES ('Task 5', '', 3, 2);
+
+--> statement-breakpoint
+INSERT INTO tasks (title, description, project_id, owner_id)
+    VALUES ('Task 6', '', 3, 2);
+
+--> statement-breakpoint
+INSERT INTO tasks (title, description, project_id, owner_id)
+    VALUES ('Task 7', '', 4, 2);
+
+--> statement-breakpoint
+INSERT INTO tasks (title, description, project_id, owner_id)
+    VALUES ('Task 8', '', 4, 2);
+
