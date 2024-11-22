6682a4e6855c07315e84778fc415c64e6ea78dfa
Emanuel Turis <hello@emanuelturis.com>
2024-11-22 15:59:12 +0200

    update database schema and methods for deleting sessions

--- database/migrations/000001_initial.up.sql
+++ database/migrations/000001_initial.up.sql
@@ -12,8 +12,10 @@ CREATE UNIQUE INDEX users_email_idx ON users (email);
 --> statement-breakpoint
 CREATE TABLE "sessions" (
     "id" serial PRIMARY KEY,
-    "token" text NOT NULL,
     "user_id" integer NOT NULL,
+    "token" text NOT NULL,
+    "user_agent" text NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT now(),
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
--- database/session.go
+++ database/session.go
@@ -3,13 +3,16 @@ package database
 import (
 	"database/sql"
 
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jmoiron/sqlx"
 )
 
 type Session struct {
-	ID     int32  `db:"id"`
-	UserID int32  `db:"user_id"`
-	Token  string `db:"token"`
+	ID        int32            `db:"id"`
+	UserID    int32            `db:"user_id"`
+	Token     string           `db:"token"`
+	UserAgent string           `db:"user_agent"`
+	CreatedAt pgtype.Timestamp `db:"created_at"`
 }
 
 type SessionService struct {
@@ -35,11 +38,17 @@ func (ss SessionService) GetSessionByToken(token string) (Session, bool, error) 
 	return Session{}, false, nil
 }
 
-func (ss SessionService) CreateToken(tx *sqlx.Tx, userId int32, token string) error {
+func (ss SessionService) CreateToken(
+	tx *sqlx.Tx,
+	userId int32,
+	token string,
+	userAgent string,
+) error {
 	_, err := tx.Exec(
-		"insert into sessions (user_id, token) values ($1, $2);",
+		"insert into sessions (user_id, token, user_agent) values ($1, $2, $3);",
 		userId,
 		token,
+		userAgent,
 	)
 	if err != nil {
 		return err
@@ -56,4 +65,29 @@ func (ss SessionService) DeleteToken(token string) error {
 		return err
 	}
 	return nil
+}
+
+func (ss SessionService) DeleteAllTokens(userId int32, token string) error {
+	_, err := ss.db.Exec(
+		"delete from sessions where id in (select sessions.id from sessions left join users on users.id = sessions.user_id where users.id = $1 and sessions.token != $2);",
+		userId,
+		token,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ss SessionService) GetAllSessions(userId int32) ([]Session, error) {
+	var sessions []Session
+	err := ss.db.Select(
+		&sessions,
+		"select sessions.* from sessions left join users on users.id = sessions.user_id where users.id = $1;",
+		userId,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return sessions, nil
 }
